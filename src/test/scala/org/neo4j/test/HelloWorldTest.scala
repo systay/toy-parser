73a2396120bc38c911f4814fdee73199c42a9caa
Andres Taylor <andres@neotechnology.com>
2012-02-03 14:13:00 +0100

    this

--- src/test/scala/org/neo4j/test/HelloWorldTest.scala
+++ src/test/scala/org/neo4j/test/HelloWorldTest.scala
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2002-2012 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.test
 
 import org.scalatest.Assertions
@@ -53,27 +72,5 @@ class HelloWorldTest extends Parsers with Assertions {
     assertThat(parsed, instanceOf(classOf[Failure]))
     println(parsed)
   }
-
-
-  //  @Test def twoTokensWithAnd() {
-  //    val parser = new Then(new Token('a'), new Token('b'))
-  //    val result = parser.apply(new Input("ab", 0))
-  //    println(result)
-  //  }
-
-
-  //  class Token(token: Char) extends Parser[String] {
-  //    def apply(v1: Input) = if (v1.first == token) Success(token.toString, v1.rest) else Failure("wut", v1)
-  //  }
-  //
-  //  case class Then[T, R](a: Parser[T], b: Parser[R]) extends Parser[(T, R)] {
-  //    def apply(value: Input) = a(value) match {
-  //      case x: Failure => x
-  //      case Success(aVal, temp) => b(temp) match {
-  //        case Success(bVal, rest) => Success((aVal, bVal), rest)
-  //        case x: Failure => x
-  //      }
-  //    }
-  //  }
 }
 
